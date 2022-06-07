d895b530b92241983cac6acd96901cca85467e28
Dom Patmore <dom@pilifter.com>
2022-06-07 16:14:35 +0100

    adding first passing test

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -9,8 +9,40 @@
 # [ ] It should be painted with 7 colours (ROYGBIV)
 
 describe 'my robot' do
-  it 'is giant' do
-    robot = Robot.new
-    expect(robot.height).to be_greater_than(100)
-  end  
+  describe 'dimensions' do
+    it 'is really tall' do
+      robot = Robot.new
+      expect(robot.height_in_meters).to be > 100
+    end
+
+    it 'is really heavy' do
+      robot = Robot.new
+      expect(robot.weight_in_tonnes).to be >= 100
+    end
+  end
+
+  describe 'flying' do
+    it 'responds to fly' do
+      robot = Robot.new
+      expect{ robot.fly }.to_not raise_error
+    end
+
+    xit 'is airborne when told to fly' do
+      # robot = Robot.new
+      # robot.fly
+      # expect(robot.status).to be :airborne
+    end
+  end
+end
+
+class Robot
+  def height_in_meters
+    101
+  end
+
+  def weight_in_tonnes
+    100
+  end
+
+  def fly; end
 end
